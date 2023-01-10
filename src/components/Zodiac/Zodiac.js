1e02daf8aecbf7e8035dc9bc1ab04589606b0e55
kevinnail <fumalicious@gmail.com>
2023-01-09 21:52:19 -0800

    added necessary sounds

--- src/components/Zodiac/Zodiac.js
+++ src/components/Zodiac/Zodiac.js
@@ -1,8 +1,14 @@
 import './Zodiac.css';
 
 export default function Zodiac(props) {
+  let audio = new Audio(`/zodiacs/satan.wav`);
+  let audio2 = new Audio(`/zodiacs/laugh.wav`);
+  const start = () => {
+    audio.play();
+    audio2.play();
+  };
   return (
-    <div className="zodiac">
+    <div onClick={start} className="zodiac">
       <img src={`/zodiacs/${props.name}.png`} />
       <h1>{props.name}</h1>
       <h2>{props.dates}</h2>
