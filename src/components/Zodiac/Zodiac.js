a2095024a7478f83485ff03dc55079be650d7508
kevinnail <fumalicious@gmail.com>
2023-01-09 21:34:54 -0800

    images with names corrected displaying in zodiac component

--- src/components/Zodiac/Zodiac.js
+++ src/components/Zodiac/Zodiac.js
@@ -3,7 +3,7 @@ import './Zodiac.css';
 export default function Zodiac(props) {
   return (
     <div className="zodiac">
-      <img />
+      <img src={`/zodiacs/${props.name}.png`} />
       <h1>{props.name}</h1>
       <h2>{props.dates}</h2>
     </div>
