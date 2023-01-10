624ac49a6dcdb729ab17f66d81d7b49fb8c97cf7
kevinnail <fumalicious@gmail.com>
2023-01-09 17:51:39 -0800

    footer displaying properly

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
-// import Footer from './components/Footer/Footer';
+import Footer from './components/Footer/Footer';
 // import Main from './components/Main/Main';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      {/* <Main /> 
-      <Footer /> */}
+      {/* <Main />  */}
+      <Footer />
     </div>
   );
 }
