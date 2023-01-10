43a42fb9ccbd8d77770a67ed72f11ff5da09aa29
kevinnail <fumalicious@gmail.com>
2023-01-09 17:40:41 -0800

    header displaying

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.css';
+
+export default function Header() {
+  return (
+    <header>
+      <h1>Incorrect But Interesting Astrology Signs</h1>
+    </header>
+  );
+}
