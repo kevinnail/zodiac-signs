2b10dbd59044483d7a8dc74772f267a86ce41d32
kevinnail <fumalicious@gmail.com>
2023-01-09 18:15:32 -0800

    dummy text in main displaying

---
+++ src/components/Main/Main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Main.css';
+
+export default function Main() {
+  return (
+    <main>
+      <p>We are all going to die! Sleep Tight!</p>
+    </main>
+  );
+}
